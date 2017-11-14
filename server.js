aaa410b2fc256200c3732d13a03d24b6dd357785
Peter Nguyen <tronghieubk@gmail.com>
2017-11-14 13:24:12 +0700

    Update server.js

--- server.js
+++ server.js
@@ -38,6 +38,7 @@ cors_proxy.createServer({
   httpProxyOptions: {
     // Do not add X-Forwarded-For, etc. headers, because Heroku already adds it.
     xfwd: false,
+    secure: false,
   },
 }).listen(port, host, function() {
   console.log('Running CORS Anywhere on ' + host + ':' + port);
